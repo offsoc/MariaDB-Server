MariaDB/server

MDEV-34860 Implement MAX_EXECUTION_TIME hint
It places a limit N (a timeout value in milliseconds) on how long
a statement is permitted to execute before the server terminates it.

Syntax:
SELECT /*+ MAX_EXECUTION_TIME(milliseconds) */ ...

Only top-level SELECT statements support the hint.

sql/opt_hints_parser.cc
@@ -57,12 +57,17 @@ Optimizer_hint_tokenizer::find_keyword(const LEX_CSTRING &str)
       return TokenID::keyword_QB_NAME;
     break;
 
+  case 18:
+    if ("MAX_EXECUTION_TIME"_Lex_ident_column.streq(str))
+      return TokenID::keyword_MAX_EXECUTION_TIME;
+    break;
+
   case 21:
     if ("NO_RANGE_OPTIMIZATION"_Lex_ident_column.streq(str))
       return TokenID::keyword_NO_RANGE_OPTIMIZATION;
     break;
   }
-  return TokenID::tIDENT;
+  return TokenID::tIDENTorNUMBER;
 }
 
 
@@ -86,7 +91,7 @@ Optimizer_hint_tokenizer::get_token(CHARSET_INFO *cs)
       all identifiers in the same way in the hint parser.
     */
     if (delimited_ident.length > 2)
-      return Token(delimited_ident, TokenID::tIDENT);
+      return Token(delimited_ident, TokenID::tIDENTorNUMBER);
     /*
       If the string is empty, "unget" it to have a good
       syntax error position in the message text.
@@ -100,7 +105,7 @@ Optimizer_hint_tokenizer::get_token(CHARSET_INFO *cs)
   const Token_with_metadata ident= get_ident();
   if (ident.length)
     return Token(ident, ident.m_extended_chars ?
-                 TokenID::tIDENT : find_keyword(ident));
+                 TokenID::tIDENTorNUMBER : find_keyword(ident));
   if (!get_char(','))
     return Token(Lex_cstring(m_ptr - 1, 1), TokenID::tCOMMA);
   if (!get_char('@'))

offsoc/MariaDB-Server

sql/opt_hints_parser.cc
@@ -57,10 +57,29 @@ Optimizer_hint_tokenizer::find_keyword(const LEX_CSTRING &str)
       return TokenID::keyword_QB_NAME;
     break;
 
+  case 18:
+    if ("MAX_EXECUTION_TIME"_Lex_ident_column.streq(str))
+      return TokenID::keyword_MAX_EXECUTION_TIME;
+    break;
+
   case 21:
     if ("NO_RANGE_OPTIMIZATION"_Lex_ident_column.streq(str))
       return TokenID::keyword_NO_RANGE_OPTIMIZATION;
     break;
+  }
+
+  if (str.length > 0 && (str.str[0] >= '0' && str.str[0] <= '9'))
+  {
+    /*
+      If all characters are digits, qualify the token as a number,
+      otherwise as an identifier
+    */
+    for(size_t i = 1; i < str.length; i++)
+    {
+      if (str.str[i] < '0' || str.str[i] > '9')
+        return TokenID::tIDENT;
+    }
+    return TokenID::tUNSIGNED_NUMBER;
   }
   return TokenID::tIDENT;
 }