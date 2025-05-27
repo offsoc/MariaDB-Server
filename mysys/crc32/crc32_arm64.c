7cfb88360e8f288ae1966017031bf7249f08abd6
Vladislav Vaintroub <vvaintroub@gmail.com>
2025-05-27 15:44:03 +0200

    test

--- mysys/crc32/crc32_arm64.c
+++ mysys/crc32/crc32_arm64.c
@@ -9,7 +9,7 @@ unsigned crc32_aarch64(unsigned, const void *, size_t);
 #ifdef HAVE_ARMV8_CRC
 
 # ifdef HAVE_ARMV8_CRYPTO
-static unsigned crc32c_aarch64_pmull(unsigned, const void *, size_t);
+static unsigned int crc32c_aarch64_pmull(unsigned, const void *, size_t);
 # endif
 
 # ifdef _WIN32
@@ -29,7 +29,9 @@ my_crc32_t crc32c_aarch64_available(void)
 {
   if (crc32_aarch64_available() == 0)
     return NULL;
-  /* TODO : pmull seems supported, but does not compile*/
+
+  if (IsProcessorFeaturePresent(PF_ARM_V8_CRYPTO_INSTRUCTIONS_AVAILABLE))
+    return crc32c_aarch64_pmull;
   return crc32c_aarch64;
 }
 
@@ -181,11 +183,19 @@ asm(".arch_extension crypto");
   CRC32C3X8(buffer, ((ITR) * 7 + 6)) \
 } while(0)
 
+#ifdef _MSC_VER
+#define PREF4X64L1(buffer, offset, itr)\
+  __prefetch((const char *)buffer + (offset) + ((itr) + 0)*64);\
+  __prefetch((const char *)buffer + (offset) + ((itr) + 1)*64);\
+  __prefetch((const char *)buffer + (offset) + ((itr) + 2)*64);\
+  __prefetch((const char *)buffer + (offset) + ((itr) + 3)*64);
+#else
 #define PREF4X64L1(buffer, PREF_OFFSET, ITR) \
   __asm__("PRFM PLDL1KEEP, [%x[v],%[c]]"::[v]"r"(buffer), [c]"I"((PREF_OFFSET) + ((ITR) + 0)*64));\
   __asm__("PRFM PLDL1KEEP, [%x[v],%[c]]"::[v]"r"(buffer), [c]"I"((PREF_OFFSET) + ((ITR) + 1)*64));\
   __asm__("PRFM PLDL1KEEP, [%x[v],%[c]]"::[v]"r"(buffer), [c]"I"((PREF_OFFSET) + ((ITR) + 2)*64));\
   __asm__("PRFM PLDL1KEEP, [%x[v],%[c]]"::[v]"r"(buffer), [c]"I"((PREF_OFFSET) + ((ITR) + 3)*64));
+#endif
 
 #define PREF1KL1(buffer, PREF_OFFSET) \
   PREF4X64L1(buffer,(PREF_OFFSET), 0) \
@@ -193,11 +203,15 @@ asm(".arch_extension crypto");
   PREF4X64L1(buffer,(PREF_OFFSET), 8) \
   PREF4X64L1(buffer,(PREF_OFFSET), 12)
 
+#ifdef _MSC_VER
+#define PREF4X64L2(a,b,c)
+#else
 #define PREF4X64L2(buffer, PREF_OFFSET, ITR) \
   __asm__("PRFM PLDL2KEEP, [%x[v],%[c]]"::[v]"r"(buffer), [c]"I"((PREF_OFFSET) + ((ITR) + 0)*64));\
   __asm__("PRFM PLDL2KEEP, [%x[v],%[c]]"::[v]"r"(buffer), [c]"I"((PREF_OFFSET) + ((ITR) + 1)*64));\
   __asm__("PRFM PLDL2KEEP, [%x[v],%[c]]"::[v]"r"(buffer), [c]"I"((PREF_OFFSET) + ((ITR) + 2)*64));\
   __asm__("PRFM PLDL2KEEP, [%x[v],%[c]]"::[v]"r"(buffer), [c]"I"((PREF_OFFSET) + ((ITR) + 3)*64));
+#endif
 
 #define PREF1KL2(buffer, PREF_OFFSET) \
   PREF4X64L2(buffer,(PREF_OFFSET), 0) \
@@ -240,6 +254,23 @@ static unsigned crc32c_aarch64(unsigned crc, const void *buf, size_t len)
 #endif
 
 #ifdef HAVE_ARMV8_CRYPTO
+static inline uint64_t poly64_mul(uint64_t a_scalar, uint64_t b_scalar)
+{
+#ifdef _MSC_VER
+  __n64 a= vcreate_p64(a_scalar);
+  __n64 b= vcreate_p64(b_scalar);
+
+  __n128 res= neon_pmull_64(a, b);
+
+  uint64x2_t res_u64= vreinterpretq_u64_p128(res);
+
+  // For example, return the low 64 bits
+  return vgetq_lane_u64(res_u64, 0);
+#else /* __GNUC__ */
+  return (uint64_t) vmull_p64(crc1, k2);
+#endif
+}
+
 static unsigned crc32c_aarch64_pmull(unsigned crc, const void *buf, size_t len)
 {
   int64_t length= (int64_t)len;
@@ -286,8 +317,8 @@ static unsigned crc32c_aarch64_pmull(unsigned crc, const void *buf, size_t len)
      * crc1 multiply by K2
      * crc0 multiply by K1
      */
-    t1= (uint64_t)vmull_p64(crc1, k2);
-    t0= (uint64_t)vmull_p64(crc0, k1);
+    t1= poly64_mul(crc1, k2);
+    t0= poly64_mul(crc0, k1);
     crc= __crc32cd(crc2, *(const uint64_t *)buffer);
     crc1= __crc32cd(0, t1);
     crc^= crc1;
