MariaDB/server

MDEV-36132 Substitute vcol expressions with indexed vcol fields in ORDER BY
Also expand vcol field index coverings to include indexes covering all
the fields in the expression. The reasoning goes as follows: let f(c1,
c2, ..., cn) be a function on applied to columns c1, c2, ..., cn, if
f(...) is covered by an index, so should vc whose expression is
f(...).

For example, if t.vf = t.c1 + t.c2, and t has three indexes (vf), (c1,
c2), (c1).

Before this change, vf's index covering is a singleton {(vf)}. Let's call
that the "conventional" index covering.

After this change vf's index covering is now {(vf), (c1, c2)}, since
(c1, c2) covers both c1 and c2. Let's call (c1, c2) in this case the
"extra" covering.

With the coverings updated, when an index in the "extra" covering is
chosen for keyread, the vcol also needs to be calculated. In this case
we mark vcol in the table read_set, and ensure it is computed.

With these changes, we see various improvements, including from using
full table scan + filesort to full index scan + filesort when ORDER BY
an indexed vcol (here vc = c + 1 is a vcol and both c and vc are
indexes):

 explain select c + 1 from t order by vc;
 id	select_type	table	type	possible_keys	key	key_len	ref	rows	Extra
-1	SIMPLE	t	ALL	NULL	NULL	NULL	NULL	10000	Using filesort
+1	SIMPLE	t	index	NULL	c	5	NULL	10000	Using index; Using filesort

sql/field.h
@@ -824,6 +824,14 @@ public:
   ha_field_option_struct *option_struct;   /* structure with parsed options */
   /* Field is part of the following keys */
   key_map	key_start, part_of_key, part_of_key_not_clustered;
+  /*
+    If the field is a vcol, its part_of_key not only contain keys that
+    have vcol as parts ("conventional"), but also keys with vcol
+    expression fields as parts ("extra"), computed from
+    `intersect_vcol_index_coverings'. The field `vcol_part_of_key' is
+    the "conventional" part_of_key.
+  */
+  key_map vcol_part_of_key;
 
   /*
     Bitmap of indexes that have records ordered by col1, ... this_field, ...

offsoc/MariaDB-Server

sql/field.h
@@ -824,6 +824,14 @@ public:
   ha_field_option_struct *option_struct;   /* structure with parsed options */
   /* Field is part of the following keys */
   key_map	key_start, part_of_key, part_of_key_not_clustered;
+  /*
+    If the field is a vcol, its part_of_key not only contain keys that
+    have vcol as parts ("conventional"), but also keys with vcol
+    expression fields as parts ("extra"), computed from
+    `intersect_field_part_of_key'. The field `vcol_part_of_key' is
+    the "conventional" part_of_key.
+  */
+  key_map vcol_part_of_key;
 
   /*
     Bitmap of indexes that have records ordered by col1, ... this_field, ...